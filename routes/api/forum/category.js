bc4c13ac3ce7d88fbd613e95f63b489a1f0bae02
Mark Kohler <markkohler3@gmail.com>
2019-08-13 08:53:33 -0400

    Added forum routes

--- routes/api/forum/category.js
+++ routes/api/forum/category.js
@@ -1,8 +1,34 @@
 import { Router } from "express";
 import authorization from "../../../middleware/api/authorization";
 import updateLastOnline from "../../../middleware/api/updateLastOnline";
+import adminOnly from "../../../middleware/api/adminOnly";
 import Category from "../../../models/Category";
 
 const router = Router();
+
+/* All category routes are protected by the admin middleware in addition to the normal middleware */
+router.use("/", authorization);
+router.use("/", updateLastOnline);
+router.use("/", adminOnly);
+
+// @route   POST /forum/category
+// @desc    Create a new category
+// @access  Private
+router.post("/", (req, res) => {});
+
+// @route   GET /forum/category/:id
+// @desc    Retrieves the given category
+// @access  Private
+router.get("/:id", (req, res) => {});
+
+// @route   DELETE /forum/category/:id
+// @desc    Deletes the given category
+// @access  Private
+router.delete("/:id", (req, res) => {});
+
+// @route   UPDATE /forum/category/:id
+// @desc    Edits the given category
+// @access  Private
+router.update("/:id", (req, res) => {});
 
 export default router;
--- routes/api/forum/post.js
+++ routes/api/forum/post.js
@@ -4,5 +4,26 @@ import updateLastOnline from "../../../middleware/api/updateLastOnline";
 import Post from "../../../models/Post";
 
 const router = Router();
+router.use("/", authorization);
+router.use("/", updateLastOnline);
 
+// @route   POST /forum/post
+// @desc    Create a new post
+// @access  Private
+router.post("/", (req, res) => {});
+
+// @route   GET /forum/post/:id
+// @desc    Retrieves the given post
+// @access  Private
+router.get("/:id", (req, res) => {});
+
+// @route   DELETE /forum/post/:id
+// @desc    Deletes the given post
+// @access  Private
+router.delete("/:id", (req, res) => {});
+
+// @route   UPDATE /forum/post/:id
+// @desc    Edits the given post
+// @access  Private
+router.update("/:id", (req, res) => {});
 export default router;
