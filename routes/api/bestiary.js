ecb54fc0a507c454a4fa8aef50eef1ee9a7a993c
Mark Kohler <markkohler3@gmail.com>
2019-08-22 11:18:46 -0400

    Bestiary router finished

--- routes/api/bestiary.js
+++ routes/api/bestiary.js
@@ -13,67 +13,67 @@ router.use('/', updateLastOnline);
 // @desc    Returns a list of beasts
 // @access  Private
 router.get('/', Cache.retrieve, async (req, res) => {
-	/* Pagination is done on the server side */
-	const count = parseInt(req.query.count || 10);
-	const page = (req.query.page || 1) - 1;
-	const toSkip = page * count;
-	const totalDocs = await Beast.estimatedDocumentCount();
-	const pages = totalDocs / count;
-	const sortOrder = req.query.sortOrder || 1;
-	const sortCol = req.query.sortColumn || '_id';
+  /* Pagination is done on the server side */
+  const count = parseInt(req.query.count || 10);
+  const page = (req.query.page || 1) - 1;
+  const toSkip = page * count;
+  const totalDocs = await Beast.estimatedDocumentCount();
+  const pages = totalDocs / count;
+  const sortOrder = req.query.sortOrder || 1;
+  const sortCol = req.query.sortColumn || '_id';
 
-	Beast.find({}, 'name shortDesc longDesc _id', {
-		skip: toSkip,
-		limit: count,
-		sort: { [sortCol]: sortOrder },
-	})
-		.then(beasts => {
-			Cache.cache(3600)(req, res, {
-				success: true,
-				page: page + 1,
-				numberPerPage: count,
-				pages,
-				sortColumn: sortCol,
-				ascending: sortOrder === 1,
-				lastPage: page + 1 >= pages,
-				message: 'Monsters retrieved successfully',
-				beasts,
-			});
-		})
-		.catch(e => {
-			res.status(500).json({
-				success: false,
-				message: e.message || 'An unknown error occured',
-			});
-		});
+  Beast.find({}, 'name shortDesc longDesc _id', {
+    skip: toSkip,
+    limit: count,
+    sort: { [sortCol]: sortOrder }
+  })
+    .then(beasts => {
+      Cache.cache(3600)(req, res, {
+        success: true,
+        page: page + 1,
+        numberPerPage: count,
+        pages,
+        sortColumn: sortCol,
+        ascending: sortOrder === 1,
+        lastPage: page + 1 >= pages,
+        message: 'Monsters retrieved successfully',
+        beasts
+      });
+    })
+    .catch(e => {
+      res.status(500).json({
+        success: false,
+        message: e.message || 'An unknown error occured'
+      });
+    });
 });
 
 // @route   GET /bestiary/:id
 // @desc    Returns the given beast
 // @access  Private
 router.get('/:id', (req, res) => {
-	try {
-		Beast.findById(req.params.id).then(beast => {
-			if (!beast)
-				res
-					.status(403)
-					.json({ sucess: false, message: 'Resource was not found' });
-			res
-				.json({
-					success: true,
-					message: 'entry successfully retreived',
-					beast,
-				})
-				.catch(e => {
-					res.status(500).json({
-						success: false,
-						message: e.message || 'An unknown error occured',
-					});
-				});
-		});
-	} catch (e) {
-		res.status(404).json({ success: false, message: 'Entry not found' });
-	}
+  try {
+    Beast.findById(req.params.id).then(beast => {
+      if (!beast)
+        res
+          .status(403)
+          .json({ sucess: false, message: 'Resource was not found' });
+      res
+        .json({
+          success: true,
+          message: 'entry successfully retreived',
+          beast
+        })
+        .catch(e => {
+          res.status(500).json({
+            success: false,
+            message: e.message || 'An unknown error occured'
+          });
+        });
+    });
+  } catch (e) {
+    res.status(404).json({ success: false, message: 'Entry not found' });
+  }
 });
 
 router.use('/', adminOnly);
@@ -82,36 +82,75 @@ router.use('/', adminOnly);
 // @desc    Create a new beast
 // @access  Private
 router.post('/', (req, res) => {
-	new Beast({
-		name: req.body.name,
-		shortDesc: req.body.shortDesc,
-		longDesc: req.body.longDesc,
-		images: req.body.images,
-	})
-		.save()
-		.then(beast =>
-			res.json({
-				success: true,
-				message: 'Monster created successfully',
-				beast,
-			})
-		)
-		.catch(e =>
-			res.status(500).json({
-				success: false,
-				message: e.message || 'An unknown error occured',
-			})
-		);
+  new Beast({
+    name: req.body.name,
+    shortDesc: req.body.shortDesc,
+    longDesc: req.body.longDesc,
+    images: req.body.images
+  })
+    .save()
+    .then(beast =>
+      res.json({
+        success: true,
+        message: 'Monster created successfully',
+        beast
+      })
+    )
+    .catch(e =>
+      res.status(500).json({
+        success: false,
+        message: e.message || 'An unknown error occured'
+      })
+    );
 });
 
 // @route   PUT /bestiary/:id
 // @desc    Update the given beast
 // @access  Private
-router.put('/:id', (req, res) => {});
+router.put('/:id', (req, res) => {
+  Beast.findById(req.params.id)
+    .then(beast => {
+      beast.name = req.body.name || beast.name;
+      beast.shortDesc = req.body.shortDesc || beast.shortDesc;
+      beast.longtDesc = req.body.longDesc || beast.longDesc;
+      beast.images = req.body.images || beast.images;
+      beast
+        .save()
+        .then(beast =>
+          res.json({
+            success: true,
+            message: 'Monster updated successfully',
+            beast
+          })
+        )
+        .catch(e =>
+          res
+            .status(500)
+            .json({ success: false, message: 'An unkown error occured' })
+        );
+    })
+    .catch(e =>
+      res.status(404).json({
+        success: false,
+        message: 'The requested resource was not found on the server'
+      })
+    );
+});
 
 // @route   DELETE /bestiary/:id
 // @desc    Delete the given beast
 // @access  Private
-router.delete('/:id', (req, res) => {});
+router.delete('/:id', (req, res) => {
+  Beast.findByIdAndDelete(req.params.id)
+    .then(() =>
+      res.json({ success: true, message: 'Monster deleted successfylly' })
+    )
+    .catch(e =>
+      res.status(404).json({
+        success: false,
+        message: 'The requested resource was not found on the server'
+      })
+    );
+});
 
 export default router;
