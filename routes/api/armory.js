ea2eb1815cfbf243c8d20a8a4fa3131862c7fbe9
Mark Kohler <markkohler3@gmail.com>
2019-08-22 11:16:17 -0400

    Items route finished

--- routes/api/armory.js
+++ routes/api/armory.js
@@ -13,32 +13,69 @@ router.use('/', updateLastOnline);
 // @route   GET /armory
 // @desc    Returns a list of equipment
 // @access  Private
-router.get('/', Cache.retrieve, (req, res) => {});
+router.get('/', Cache.retrieve, async (req, res) => {
+  /* Get values from query parameters */
+  const count = parseInt(req.query.count || 10);
+  const page = (req.query.page || 1) - 1;
+  const toSkip = page * count;
+  const sortOrder = req.query.sortOrder || 1;
+  const sortCol = req.query.sortColumn || '_id';
+
+  /* Retrieves a more complete list if the user is an admin */
+
+  const totalDocs = await Item.estimatedDocumentCount();
+  const pages = Math.ceil(totalDocs / count);
+
+  Item.find({}, '', {
+    skip: toSkip,
+    limit: count,
+    sort: { [sortCol]: sortOrder }
+  })
+    .then(items => {
+      Cache.cache(3600)(req, res, {
+        success: true,
+        page: page + 1,
+        numberPerPage: count,
+        pages,
+        sortCol,
+        ascending: sortOrder === 1,
+        lastPage: page + 1 >= pages,
+        message: 'Items retrieved successfully',
+        items
+      });
+    })
+    .catch(e => {
+      res.status(500).json({
+        success: false,
+        message: e.message || 'An unknown error occured'
+      });
+    });
+});
 
 // @route   GET /armory/:id
 // @desc    Returns the given item
 // @access  Private
 router.get('/:id', Cache.retrieve, (req, res) => {
-	try {
-		Item.findById(req.params.id).then(item => {
-			if (!item)
-				return res
-					.status(403)
-					.json({ sucess: false, message: 'Resource was not found' });
-			Cache.cache(3600)(req, res, {
-				success: true,
-				message: 'entry successfully retreived',
-				item,
-			}).catch(e => {
-				res.status(500).json({
-					success: false,
-					message: e.message || 'An unknown error occured',
-				});
-			});
-		});
-	} catch (e) {
-		res.status(404).json({ success: false, message: 'Entry not found' });
-	}
+  try {
+    Item.findById(req.params.id).then(item => {
+      if (!item)
+        return res
+          .status(403)
+          .json({ sucess: false, message: 'Resource was not found' });
+      Cache.cache(3600)(req, res, {
+        success: true,
+        message: 'entry successfully retreived',
+        item
+      }).catch(e => {
+        res.status(500).json({
+          success: false,
+          message: e.message || 'An unknown error occured'
+        });
+      });
+    });
+  } catch (e) {
+    res.status(404).json({ success: false, message: 'Entry not found' });
+  }
 });
 
 router.use('/', adminOnly);
@@ -46,16 +83,69 @@ router.use('/', adminOnly);
 // @route   POST /armory
 // @desc    Creates a new item
 // @access  Private
-router.post('/', (req, res) => {});
+router.post('/', (req, res) => {
+  new Item({})
+    .save()
+    .then(item =>
+      res.json({ success: true, message: 'Item created successfully', item })
+    )
+    .catch(e =>
+      res.status(400).json({
+        success: false,
+        message: e.message || 'An unknown error occured'
+      })
+    );
+});
 
 // @route   PUT /armory
 // @desc    Updates the given item
 // @access  Private
-router.put('/:id', (req, res) => {});
+router.put('/:id', (req, res) => {
+  Item.findById(req.params.id)
+    .then(item => {
+      item.name = req.body.name || item.name;
+      item.shortDesc = req.body.shortDesc || item.shortDesc;
+      item.longtDesc = req.body.longDesc || item.longDesc;
+      item.images = req.body.images || item.images;
+      item
+        .save()
+        .then(item =>
+          res.json({
+            success: true,
+            message: 'Item updated successfully',
+            item
+          })
+        )
+        .catch(e =>
+          res
+            .status(500)
+            .json({ success: false, message: 'An unkown error occured' })
+        );
+    })
+    .catch(e =>
+      res.status(404).json({
+        success: false,
+        message: 'The requested resource was not found on the server'
+      })
+    );
+});
 
 // @route   DELETE /armory
 // @desc    Deletes the given item
 // @route access
-router.delete('/:id', (req, res) => {});
+router.delete('/:id', (req, res) => {
+  Item.findByIdAndDelete(req.params.id)
+    .then(() =>
+      res.json({ success: true, message: 'Item deleted successfylly' })
+    )
+    .catch(e =>
+      res
+        .status(404)
+        .json({
+          success: false,
+          message: 'The requested resource was not found on the server'
+        })
+    );
+});
 
 export default router;
